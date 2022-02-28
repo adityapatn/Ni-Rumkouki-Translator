73c69cd40d5803e9af8405ecd3e64e3db2244dd1
adityapatn <68922417+adityapatn@users.noreply.github.com>
2022-02-27 17:21:50 -0800

    First Commit Second Project

--- App.js
+++ App.js
@@ -1,12 +1,99 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform, View, Alert } from 'react-native';
+import { Button, Text, Input } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import translateNiRumkouki from './NiRumkoukiScript';
 
 export default function App() {
+  const [translatedText, setTranslatedText] = useState("English/Ni Rumkouki Text will appear here")
+  const [preTranslatedText, setPreTranslatedText] = useState("default value")
+
+  const translate = () => {
+    setTranslatedText(translateNiRumkouki(preTranslatedText));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <Text
+              style={styles.text}
+              h3
+            >
+              Ni Rumkouki Translator
+            </Text>
+            <Button
+              title={'Take a Picture of Text'}
+              containerStyle={{
+                width: 300,
+                marginHorizontal: 50,
+                marginVertical: 10,
+              }}
+              icon={{
+                name: 'camera',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              iconContainerStyle={{ marginRight: 10 }}
+            />
+            <Input 
+              multiline={true}
+              numberOfLines={4}
+              placeholder="Enter Ni Rumkouki/English Text"
+              containerStyle={{ height: 200, textAlignVertical: 'top', borderWidth: 1, width: 300, }}
+              inputContainerStyle={{borderBottomWidth:0}}
+              value={preTranslatedText}
+              onChangeText={setPreTranslatedText}
+            />
+            <Button
+              onPress = {translate}
+              title={'Translate Text'}
+              containerStyle={{
+                width: 300,
+                marginHorizontal: 50,
+                marginVertical: 10,
+              }}
+              icon={{
+                name: 'angle-double-down',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              iconContainerStyle={{ marginRight: 10 }}
+            />
+            <Text
+              placeholder="English/Ni Rumkouki Text will appear here"
+              multiline={true}
+              numberOfLines={4}
+              style={{ height: 200, textAlignVertical: 'top', borderWidth: 1, width: 300, fontSize: 18, padding: 5}}
+              inputContainerStyle={{borderBottomWidth:0}}
+            >{translatedText}</Text>
+            <Button
+              title={'Copy Text'}
+              containerStyle={{
+                width: 300,
+                marginHorizontal: 50,
+                marginVertical: 10,
+              }}
+              icon={{
+                name: 'copy',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              iconContainerStyle={{ marginRight: 10 }}
+            />
+            <StatusBar style="auto" />
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +102,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  text: {
+    textAlign: 'center',
+    padding: 5,
   },
 });
+
